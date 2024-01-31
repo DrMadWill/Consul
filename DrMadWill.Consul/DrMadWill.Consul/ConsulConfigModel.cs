02c73efe46b2321e4a5ab2aa5c743056c02876d4
drmadwill <nofelsalahov9@gmail.com>
2024-01-31 04:19:24 +0400

    configFix

--- DrMadWill.Consul/DrMadWill.Consul/ConsulConfigModel.cs
+++ DrMadWill.Consul/DrMadWill.Consul/ConsulConfigModel.cs
@@ -4,6 +4,13 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string[] Tags { get; set; }
+        public string ConsulAddress { get; set; }
+        public string Address { get; set; }
+        public string[] Tags => new[] { Name, Id };
+
+        public override string ToString()
+        {
+            return $" ::>> Config =>>> ID : {Id} | Name : {Name} | ConsulAddress : {ConsulAddress} | Address : {Address} ";
+        }
     }
 }
--- DrMadWill.Consul/DrMadWill.Consul/ConsulRegistration.cs
+++ DrMadWill.Consul/DrMadWill.Consul/ConsulRegistration.cs
@@ -1,7 +1,8 @@
 ﻿using Consul;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -10,46 +11,69 @@ namespace DrMadWill.Consul
 {
     public static class ConsulRegistration
     {
-        public static IServiceCollection ConfigureConsul(this IServiceCollection services, string address)
+        public static ConsulConfigModel Config;
+        public static IServiceCollection ConfigureConsul(this IServiceCollection services, IConfiguration configuration)
         {
             Console.WriteLine("IConsulClient adding");
+            Config = new ConsulConfigModel
+            {
+                Id = configuration["ConsulConfig:Id"],
+                Name = configuration["ConsulConfig:Name"],
+                ConsulAddress = configuration["ConsulConfig:ConsulAddress"],
+                Address = configuration["ConsulConfig:Address"],
+            };
+            Console.WriteLine(Config.ToString());
             services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(
-                config => config.Address = new Uri(address)));
+                config => config.Address = new Uri(Config.ConsulAddress)));
             Console.WriteLine("IConsulClient added");
             return services;
         }
 
-        public static IApplicationBuilder RegisterWithConsul(this IApplicationBuilder app, IHostApplicationLifetime lifetime, ConsulConfigModel model)
+        public static IApplicationBuilder RegisterWithConsul(this IApplicationBuilder app, IHostApplicationLifetime lifetime)
         {
-            Console.WriteLine("Registering start...");
-            var consulCilent = app.ApplicationServices.GetRequiredService<IConsulClient>();
-
-            var logFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
-
-            var logger = logFactory.CreateLogger<IApplicationBuilder>();
-
-            Console.WriteLine("Consul Client And Logger Created.");
-
-            // Get Server IP Address
-
-            var features = app.Properties["server:Features"] as FeatureCollection;
-            var addresses = features.Get<IServerAddressesFeature>();
-            var address = addresses.Addresses.First();
-            var uri = new Uri(address);
-            var registration = new AgentServiceRegistration()
+           
+            lifetime.ApplicationStarted.Register(() =>
             {
-                ID = model.Id,
-                Name = model.Name,
-                Address = $"{uri.Host}",
-                Port = uri.Port,
-                Tags = model.Tags,
-            };
+                Console.WriteLine("Registering start...");
+                var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
 
-            Console.WriteLine("Register data generated.");
-            logger.LogInformation("Registering with Consul");
-            consulCilent.Agent.ServiceDeregister(registration.ID);
-            consulCilent.Agent.ServiceRegister(registration).Wait();
-            Console.WriteLine("Registered Consul.");
+                var logFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+
+                var logger = logFactory.CreateLogger<IApplicationBuilder>();
+
+                Console.WriteLine("Consul Client And Logger Created.");
+                // Get Server IP Address
+
+                var uri = new Uri(Config.Address);
+                var registration = new AgentServiceRegistration()
+                {
+                    ID = Config.Id,
+                    Name = Config.Name,
+                    Address = $"{uri.Host}",
+                    Port = uri.Port,
+                    Tags = Config.Tags,
+                };
+
+                Console.WriteLine("Register data generated.");
+                logger.LogInformation("Registering with Consul" + Config);
+                consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+                consulClient.Agent.ServiceRegister(registration).Wait();
+                Console.WriteLine("Registered Consul.");
+                
+            });
+            
+            lifetime.ApplicationStopped.Register(() =>
+            {
+                var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+
+                var logFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+
+                var logger = logFactory.CreateLogger<IApplicationBuilder>();
+                Console.WriteLine("Deregister start" + Config);
+                logger.LogInformation("Deregister service" + Config); 
+                consulClient.Agent.ServiceDeregister(Config.Id).Wait();
+                Console.WriteLine("Deregister end. " );
+            });
             return app;
         }
     }
